82d8773133611ba77c786649ab68e8aa658d6eb7
Mehmet Metin Delibas <m.metiin7@gmail.com>
2026-06-22 06:43:53 +0300

    Dependency Injection sınıfları vev temel klasor ve katmanlar olusuturuldu

--- UniVibe/src/UniVibe.API/Program.cs
+++ UniVibe/src/UniVibe.API/Program.cs
@@ -1,6 +1,13 @@
+using UniVibe.Application;
+using UniVibe.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApplicationServices();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ UniVibe/src/UniVibe.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace UniVibe.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        { 
+            return services;
+        }
+    }
+}
