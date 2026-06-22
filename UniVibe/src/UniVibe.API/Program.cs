bcfc146ac5879430d7062e431b3d025f2f268d7d
58Adige <ibrahim58ipek@gmail.com>
2026-06-22 12:59:06 +0300

    IEntity interfacesi oluşturuldu

--- UniVibe/src/UniVibe.API/Program.cs
+++ UniVibe/src/UniVibe.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices();
 
+
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
