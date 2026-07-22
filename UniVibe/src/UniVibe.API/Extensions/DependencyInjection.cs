61596df5c1ca64cd61bef55581d0c09965e5e5d2
mehmetdelibas <m.metiin7@gmail.com>
2026-07-22 16:51:24 +0300

    fix

--- UniVibe/src/UniVibe.API/Extensions/DependencyInjection.cs
+++ UniVibe/src/UniVibe.API/Extensions/DependencyInjection.cs
@@ -21,7 +21,7 @@ namespace UniVibe.API.Extensions
                     policy.WithOrigins(
                               "http://localhost:3000",
                               "http://127.0.0.1:3000",
-                              "https://univibe-three.vercel.app/"
+                              "https://univibe-three.vercel.app"
                           )
                           .AllowAnyHeader()
                           .AllowAnyMethod()
